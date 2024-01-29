901e7bd38f26b3ee16f5020b59f8f24fe5c001c8
Illia Leedskiy <leedqvo@gmail.com>
2024-01-29 16:27:01 +0100

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,13 +9,8 @@ namespace bagtask
         public static void Main(String[] args)
         {
             Console.WriteLine("--------------------------------\n");
-            Console.WriteLine("Object oriented programming\n");
             Console.WriteLine("Bag implementation\n");
-            Console.WriteLine("Assignment 1, task 9\n");
             Console.WriteLine("Made by: Illia Takhtamyshev\n");
-            Console.WriteLine("Neptun: RP0KRP\n");
-            Console.WriteLine("Group: 06\n");
-            Console.WriteLine("E-mail: takhtamyshev17@gmail.com\n");
             Console.WriteLine("--------------------------------\n");
 
             Menu start = new Menu();
